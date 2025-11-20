4dc06f371483ee7fd3c3bffe7070b2421952159f
Sam290611 <samuel.acosta1@est.uexternado.edu.co>
2025-11-20 16:57:51 +0000

    Promedios Lider

--- TITANIC/funciones.py
+++ TITANIC/funciones.py
@@ -1,15 +1,12 @@
 import pandas as pd
 def promedios(nombre_archivo, nombre_columna):
     df= pd.read_csv(nombre_archivo)
-    if nombre_columna not in df:
+    if nombre_columna not in df.columns:
         raise  ValueError('Error al procesar la columna')
-    else:
-        def prom(x):
-            p = 0
-            for i in x:
-                p += i
-            promedio = p/len(x)
-            return promedio
-        return prom(df[nombre_columna])
-    
-promedios("/workspaces/Proyecto_Final_Grupo_9/TITANIC/test_Titanic.csv", "Age")
+    def prom(x):
+        p = 0
+        for i in x:
+            p += i
+        promedio = p/len(x)
+        return promedio
+    return prom(df[nombre_columna])
